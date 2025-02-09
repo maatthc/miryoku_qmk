594257c4ac457b1205332b8e606dde500c93c4fc
Alexandre Andrade <aleandrade@gmail.com>
2025-02-09 11:31:15 +1100

    fix: 🐛 Add return stmt

--- keyboards/beekeeb/piantor/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/manna-harbour_miryoku/keymap.c
@@ -1,4 +1,3 @@
-// Copyright 2019 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
@@ -9,30 +8,31 @@ combo_t key_combos[] = {
     COMBO(restore_window_size, RWIN(KC_UP)),
 };
 
-// Send F13-F19 when switching layers so the companion app can switch to the corresponding layout image
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
         case 0:
-        // send f13
-        SEND_STRING(SS_TAP(X_F13));
-        break;
+            // send f13
+            SEND_STRING(SS_TAP(X_F13));
+            break;
         case 4:
-        SEND_STRING(SS_TAP(X_F14));
-        break;
+            SEND_STRING(SS_TAP(X_F14));
+            break;
         case 5:
-        SEND_STRING(SS_TAP(X_F15));
-        break;
+            SEND_STRING(SS_TAP(X_F15));
+            break;
         case 6:
-        SEND_STRING(SS_TAP(X_F16));
-        break;
+            SEND_STRING(SS_TAP(X_F16));
+            break;
         case 7:
-        SEND_STRING(SS_TAP(X_F17));
-        break;
+            SEND_STRING(SS_TAP(X_F17));
+            break;
         case 8:
-        SEND_STRING(SS_TAP(X_F18));
-        break;
+            SEND_STRING(SS_TAP(X_F18));
+            break;
         case 9:
-        SEND_STRING(SS_TAP(X_F19));
-        break;
-    default: //  for any other layers
+            SEND_STRING(SS_TAP(X_F19));
+            break;
+    }
+    return state;
 }
